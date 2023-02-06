53ebfb8f101513856a635e6a8bf6d06edc78f4da
bjschoffman <bjschoffman@gmail.com>
2023-02-05 22:33:23 -0600

    Pushing to Heroku

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json', 'utf8').then((data) => 
